bf3978c04809b9e0cd37348c7071c75c0d0cf7dc
Paxson Lim <potinglim@gmail.com>
2015-02-12 23:00:50 +0800

    switching to SQLAchemy, first stop

--- refuge/config.py
+++ refuge/config.py
@@ -7,8 +7,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = "admin"
-PASSWORD = "admin"
+USERNAME = 'admin'
+PASSWORD = 'admin'
 CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
 
@@ -16,4 +16,4 @@ SECRET_KEY = 'my_precious'
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
 # the database uri
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+SQLACHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
